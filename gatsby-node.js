8c69b124d600304bbce9e9aa80daac7997b7f8de
LearningsDev <git@learnings.dev>
2021-10-27 07:10:08 -0700

    added resolver for local image node and display images

--- gatsby-node.js
+++ gatsby-node.js
@@ -63,3 +63,37 @@ exports.createPages = async function ({ actions, graphql }) {
     })
   })
 }
+
+
+
+// creating local node of graphcms image since gatsby-image doesn't display from url passed as props
+const { createRemoteFileNode } = require('gatsby-source-filesystem');
+
+exports.createResolvers = ({
+  actions: { createNode },
+  cache,
+  createNodeId,
+  createResolvers,
+  store,
+  reporter,
+}) => {
+  const resolvers = {
+    GraphCMS_Asset: {
+      node: {
+        type: `File`,
+        resolve: ({ url }, args, context, info) => {
+          return createRemoteFileNode({
+            url,
+            store,
+            cache,
+            createNode,
+            createNodeId,
+            reporter,
+          });
+        },
+      },
+    },
+  };
+
+  createResolvers(resolvers);
+}
--- src/pages/templates/blog-post.js
+++ src/pages/templates/blog-post.js
@@ -1,13 +1,16 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
 
 import Layout from "../../components/layout"
 export default function BlogPost({ data }) {
-
+  const image = data.cms.post.coverImage.node.childImageSharp.fluid.src
   return (
     <Layout>
       <div>
         <h1>{data.cms.post.title}</h1>
+        <img src={image} alt="A dinosaur" />
         <div dangerouslySetInnerHTML={{ __html: data.cms.post.content.html }} />
       </div>
     </Layout>
@@ -24,6 +27,18 @@ export const pageQuery = graphql`
         slug
         tags
         title
+        coverImage {
+          url
+          id
+          size
+          node {
+            childImageSharp {
+              fluid(maxWidth: 800) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
       }
     }
   }
